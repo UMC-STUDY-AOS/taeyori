1d79136cfccae9498359a689b10f8b4eb5c930b1
TaeryungIm <77139152+TaeryungIm@users.noreply.github.com>
2024-04-15 19:26:21 +0900

    init commit
    4/15/23 first push

---
+++ MyPracticeApplication/app/src/main/java/com/example/mypractice/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.example.mypractice
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.mypractice.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.mainPlayerCl.setOnClickListener {
+            val intent = Intent(this,SongActivity::class.java)
+            startActivity(intent)
+        }
+//        initBottomNavigation()
+    }
+
+}
---
+++ MyPracticeApplication/app/src/main/java/com/example/mypractice/SongActivity.kt
@@ -0,0 +1,43 @@
+package com.example.mypractice
+
+import android.os.Bundle
+import android.os.PersistableBundle
+import android.view.View
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import com.example.mypractice.databinding.ActivitySongBinding
+import java.util.zip.Inflater
+
+
+class SongActivity : AppCompatActivity() {
+    lateinit var binding : ActivitySongBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivitySongBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.songActivityHomeExitIvBtn.setOnClickListener{
+            finish()
+        }
+
+        binding.playbarPlayIv.setOnClickListener{
+            setPlayerStatus(false)
+        }
+        binding.playbarPauseIv.setOnClickListener{
+            setPlayerStatus(true)
+        }
+    }
+
+    fun setPlayerStatus(isPlaying : Boolean){
+        if(isPlaying){
+            binding.playbarPlayIv.visibility = View.VISIBLE
+            binding.playbarPauseIv.visibility = View.GONE
+        }else{
+            binding.playbarPlayIv.visibility = View.GONE
+            binding.playbarPauseIv.visibility = View.VISIBLE
+        }
+    }
+}
+
+
